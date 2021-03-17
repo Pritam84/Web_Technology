ff7b05782a6ca65d32410981149a91c731ddc272
Pritam84 <70715741+Pritam84@users.noreply.github.com>
2021-03-17 21:22:38 +0600

    Added

---
+++ Lab Task 04/Home.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Public Home Page</title>
+</head>
+<body>
+
+	<?php require'Welcome.php'?>
+	
+	<h1>Welcome to Company</h1>
+	
+	<?php  require'footer.php' ?>
+	
+
+</body>
+</html>
