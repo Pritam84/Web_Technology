75b7a0de881cbdabdadac9a75b49dc60f876b4b1
Pritam84 <70715741+Pritam84@users.noreply.github.com>
2021-03-25 12:31:37 +0600

    Added

---
+++ Lab Task 05/searchproduct.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+  <body>
+
+    <form method="post" action="findproduct.php">
+      <h1>SEARCH FOR PRODUCT</h1>
+      <input type="text" name="name" required/>
+      <input type="submit" name="findproduct" value="Search"/>
+    </form>
+
+
+ 
+  </body>
+</html>
