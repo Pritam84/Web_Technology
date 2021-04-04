8819ad308eb2c80e268e8c20ad58f7f452661844
Pritam84 <70715741+Pritam84@users.noreply.github.com>
2021-04-04 20:25:30 +0600

    added

---
+++ Lab task 07/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link rel="stylesheet" type="text/css" href="style4.css">
+</head>
+<body>
+
+	<div class="container">
+		<h2 class="a1">About us</h2>
+        <p class="a2">This website is very useful to all of those who are requiring any job or scholarship. Here you can find your suitable job. Contact us for queries. Phone: 01712552552, Email: alumnisystem@gmail.com</p>
+	</div>
+
+</body>
+</html>
---
+++ Lab task 07/new.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Footer</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+
+	<div>Developed by &copy; Farhan Masud</div>
+
+</body>
+</html>
